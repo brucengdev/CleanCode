3a50442f9d19a771df2436ab743165768726b007
bruce ng <brucengdev@gmail.com>
2024-03-12 13:33:07 +0700

    Added example on using comments.

---
+++ ExplainYourselfInCode/Examples.cs
@@ -0,0 +1,36 @@
+﻿namespace ExplainYourselfInCode;
+
+public class Employee
+{
+    public bool IsHourly { get; set; }
+    public int Age { get; set; }
+
+    public bool IsEligibleForFullBenefits() =>
+        IsHourly && Age > 65;
+}
+
+/**
+ * In the first example, the comment is necessary to explain what the if condition is checking
+ * But in the second example, you can see that the comment is not necessary at all, you can
+ * perfectly rewrite code to make your intention clear, without having to maintain comments.
+ */
+
+public class Examples
+{
+    public void ExampleWithComment(Employee emp)
+    {
+        //Check to see if the employee is eligible for full benefits
+        if (emp.IsHourly && emp.Age > 65)
+        {
+            //...
+        }
+    }
+
+    public void RefactoredExample(Employee emp)
+    {
+        if (emp.IsEligibleForFullBenefits())
+        {
+            //...
+        }
+    }
+}
