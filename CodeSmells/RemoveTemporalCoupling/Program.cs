c1ecced3ddb33dbfcc0af9a48fc8bab1ad915706
brucengdev <brucengdev@gmail.com>
2024-06-26 13:19:48 +0700

    Added comment.

--- CodeSmells/RemoveTemporalCoupling/Program.cs
+++ CodeSmells/RemoveTemporalCoupling/Program.cs
@@ -33,16 +33,20 @@ public class Program
     {
         Console.WriteLine("Example of temporal coupling.");
 
+        //temporal coupling is when functions depend on each other
+        //but there is no logical enforcement and they can be called in wrong order.
         var priceCal1 = new PriceCalculatorWithTemporalCoupling();
         try { 
         priceCal1.CalculatePrice(new[] {1,2,3});//throws exception
         }catch { }
 
-        //correct usage
+        //correct usage when there is temporal coupling
         priceCal1.UpdateCurrencyRates(2);
         priceCal1.CalculatePrice(new[] {1,2,3});
 
         //eliminate temporal coupling
+        //by making sure the result of one function is the input
+        //of the other, so there is no way to call them out of order.
         var priceCal2 = new PriceCalculatorWithExplicitCoupling();
         var rate = priceCal2.UpdateCurrencyRate(2);
         priceCal2.CalculatePrice(new[] { 1, 2, 3 }, rate);
